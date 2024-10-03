e70f6ae244811f36ef5c1c94cda3aded44a3ae21
Ktulha <9145140@gmail.com>
2024-10-03 12:10:58 +0300

    add main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/",methods=['GET','POST','PUT','DELETE'])
+def hello():
+    return request.content_type
+
+if __name__ == "__main__":
+    app.run()
