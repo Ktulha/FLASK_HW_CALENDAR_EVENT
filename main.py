ca9b48f1c91f0c2f22ba54d141ad748b354040c9
Ktulha <9145140@gmail.com>
2024-10-04 11:28:55 +0300

    up

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-from flask import Flask, request
-
-app = Flask(__name__)
-
-@app.route("/",methods=['GET','POST','PUT','DELETE'])
-def hello():
-    return request.content_type
+from server import app
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
